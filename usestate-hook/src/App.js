109b63d46d571afacdbd40787a4d7413010b5b8d
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-09-28 19:41:23 +0600

    updated

--- usestate-hook/src/App.js
+++ usestate-hook/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 // ? hooks can only be used in function and cannot be used in class component
@@ -11,8 +11,19 @@ import './App.css';
  */
 // ? hooks can also be used using arrow function such as
 const App = () => {
-  return <div>Hey</div>;
-}
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
+  return (
+    <div>
+      <button onClick={() => {
+        setCount(c => c + 1)
+        setCount2(c => c + 1)
+      }}>+</button>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
+    </div>
+  );
+};
 
 
 export default App;
